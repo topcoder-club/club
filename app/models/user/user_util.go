summerblue/gohub

手机 + 短信验证码登录

app/models/user/user_util.go
@@ -17,3 +17,19 @@ func IsPhoneExist(phone string) bool {
 	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
 	return count > 0
 }
+
+// GetByPhone 通过手机号来获取用户
+func GetByPhone(email string) (userModel User) {
+	database.DB.Where("phone = ?", email).First(&userModel)
+	return
+}
+
+// GetByMulti 通过 手机号/Email/用户名 来获取用户
+func GetByMulti(loginID string) (userModel User) {
+	database.DB.
+		Where("phone = ?", loginID).
+		Or("email = ?", loginID).
+		Or("name = ?", loginID).
+		First(&userModel)
+	return
+}

topcoder-club/club

app/models/user/user_util.go
@@ -17,3 +17,19 @@ func IsPhoneExist(phone string) bool {
 	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
 	return count > 0
 }
+
+// GetByPhone 通过手机号来获取用户
+func GetByPhone(phone string) (userModel User) {
+	database.DB.Where("phone = ?", phone).First(&userModel)
+	return
+}
+
+// GetByMulti 通过 手机号/Email/用户名 来获取用户
+func GetByMulti(loginID string) (userModel User) {
+	database.DB.
+		Where("phone = ?", loginID).
+		Or("email = ?", loginID).
+		Or("name = ?", loginID).
+		First(&userModel)
+	return
+}