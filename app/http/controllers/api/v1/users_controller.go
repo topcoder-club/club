summerblue/gohub

编辑个人资料

app/http/controllers/api/v1/users_controller.go
@@ -32,3 +32,22 @@ func (ctrl *UsersController) Index(c *gin.Context) {
 		"pager": pager,
 	})
 }
+
+func (ctrl *UsersController) UpdateProfile(c *gin.Context) {
+
+	request := requests.UserUpdateProfileRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdateProfile); !ok {
+		return
+	}
+
+	currentUser := auth.CurrentUser(c)
+	currentUser.Name = request.Name
+	currentUser.City = request.City
+	currentUser.Indtroduction = request.Indtroduction
+	rowsAffected := currentUser.Save()
+	if rowsAffected > 0 {
+		response.Data(c, currentUser)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
+}

app/models/user/user_model.go
@@ -11,7 +11,12 @@ import (
 type User struct {
 	models.BaseModel
 
-	Name     string `json:"name,omitempty"`
+	Name string `json:"name,omitempty"`
+
+	City          string `json:"city,omitempty"`
+	Indtroduction string `json:"indtroduction,omitempty"`
+	Avatar        string `json:"avatar,omitempty"`
+
 	Email    string `json:"-"`
 	Phone    string `json:"-"`
 	Password string `json:"-"`

app/requests/user_request.go
@@ -1,0 +1,43 @@
+package requests
+
+import (
+	"gohub/pkg/auth"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type UserUpdateProfileRequest struct {
+	Name          string `valid:"name" json:"name"`
+	City          string `valid:"city" json:"city"`
+	Indtroduction string `valid:"indtroduction" json:"indtroduction"`
+}
+
+func UserUpdateProfile(data interface{}, c *gin.Context) map[string][]string {
+
+	// 查询用户名重复时，过滤掉当前用户 ID
+	uid := auth.CurrentUID(c)
+	rules := govalidator.MapData{
+		"name":          []string{"required", "alpha_num", "between:3,20", "not_exists:users,name," + uid},
+		"indtroduction": []string{"min_cn:4", "max_cn:240"},
+		"city":          []string{"min_cn:2", "max_cn:20"},
+	}
+
+	messages := govalidator.MapData{
+		"name": []string{
+			"required:用户名为必填项",
+			"alpha_num:用户名格式错误，只允许数字和英文",
+			"between:用户名长度需在 3~20 之间",
+			"not_exists:用户名已被占用",
+		},
+		"indtroduction": []string{
+			"min_cn:描述长度需至少 4 个字",
+			"max_cn:描述长度不能超过 240 个字",
+		},
+		"city": []string{
+			"min_cn:城市需至少 2 个字",
+			"max_cn:城市不能超过 20 个字",
+		},
+	}
+	return validate(data, rules, messages)
+}

database/migrations/2022_01_12_220433_add_fields_to_user.go
@@ -1,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		City          string `gorm:"type:varchar(10);"`
+		Indtroduction string `gorm:"type:varchar(255);"`
+		Avatar        string `gorm:"type:varchar(255);default:null"`
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&User{})
+
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropColumn(&User{}, "City")
+		migrator.DropColumn(&User{}, "Indtroduction")
+		migrator.DropColumn(&User{}, "Avatar")
+	}
+
+	migrate.Add("2022_01_12_220433_add_fields_to_user", up, down)
+}

topcoder-club/club

app/http/controllers/api/v1/users_controller.go
@@ -32,3 +32,22 @@ func (ctrl *UsersController) Index(c *gin.Context) {
 		"pager": pager,
 	})
 }
+
+func (ctrl *UsersController) UpdateProfile(c *gin.Context) {
+
+	request := requests.UserUpdateProfileRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdateProfile); !ok {
+		return
+	}
+
+	currentUser := auth.CurrentUser(c)
+	currentUser.Name = request.Name
+	currentUser.City = request.City
+	currentUser.Introduction = request.Introduction
+	rowsAffected := currentUser.Save()
+	if rowsAffected > 0 {
+		response.Data(c, currentUser)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
+}

app/models/user/user_model.go
@@ -11,7 +11,12 @@ import (
 type User struct {
 	models.BaseModel
 
-	Name     string `json:"name,omitempty"`
+	Name string `json:"name,omitempty"`
+
+	City         string `json:"city,omitempty"`
+	Introduction string `json:"introduction,omitempty"`
+	Avatar       string `json:"avatar,omitempty"`
+
 	Email    string `json:"-"`
 	Phone    string `json:"-"`
 	Password string `json:"-"`

app/requests/user_request.go
@@ -1,0 +1,43 @@
+package requests
+
+import (
+	"gohub/pkg/auth"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type UserUpdateProfileRequest struct {
+	Name         string `valid:"name" json:"name"`
+	City         string `valid:"city" json:"city"`
+	Introduction string `valid:"introduction" json:"introduction"`
+}
+
+func UserUpdateProfile(data interface{}, c *gin.Context) map[string][]string {
+
+	// 查询用户名重复时，过滤掉当前用户 ID
+	uid := auth.CurrentUID(c)
+	rules := govalidator.MapData{
+		"name":         []string{"required", "alpha_num", "between:3,20", "not_exists:users,name," + uid},
+		"introduction": []string{"min_cn:4", "max_cn:240"},
+		"city":         []string{"min_cn:2", "max_cn:20"},
+	}
+
+	messages := govalidator.MapData{
+		"name": []string{
+			"required:用户名为必填项",
+			"alpha_num:用户名格式错误，只允许数字和英文",
+			"between:用户名长度需在 3~20 之间",
+			"not_exists:用户名已被占用",
+		},
+		"introduction": []string{
+			"min_cn:描述长度需至少 4 个字",
+			"max_cn:描述长度不能超过 240 个字",
+		},
+		"city": []string{
+			"min_cn:城市需至少 2 个字",
+			"max_cn:城市不能超过 20 个字",
+		},
+	}
+	return validate(data, rules, messages)
+}

database/migrations/2022_01_12_220433_add_fields_to_user.go
@@ -1,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		City         string `gorm:"type:varchar(10);"`
+		Introduction string `gorm:"type:varchar(255);"`
+		Avatar       string `gorm:"type:varchar(255);default:null"`
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&User{})
+
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropColumn(&User{}, "City")
+		migrator.DropColumn(&User{}, "Introduction")
+		migrator.DropColumn(&User{}, "Avatar")
+	}
+
+	migrate.Add("2022_01_12_220433_add_fields_to_user", up, down)
+}