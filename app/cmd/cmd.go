summerblue/gohub

命令行模式

app/cmd/cmd.go
@@ -1,0 +1,27 @@
+// Package cmd 存放程序的所有子命令
+package cmd
+
+import (
+	"gohub/pkg/helpers"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// Env 存储全局选项 --env 的值
+var Env string
+
+// RegisterGlobalFlags 注册全局选项（flag）
+func RegisterGlobalFlags(rootCmd *cobra.Command) {
+	rootCmd.PersistentFlags().StringVarP(&Env, "env", "e", "", "load .env file, example: --env=testing will use .env.testing file")
+}
+
+// RegisterDefaultCmd 注册默认命令
+func RegisterDefaultCmd(rootCmd *cobra.Command, subCmd *cobra.Command) {
+	cmd, _, err := rootCmd.Find(os.Args[1:])
+	firtArg := helpers.FirstElement(os.Args[1:])
+	if err == nil && cmd.Use == rootCmd.Use && firtArg != "-h" && firtArg != "--help" {
+		args := append([]string{subCmd.Use}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+}

topcoder-club/club

app/cmd/cmd.go
@@ -1,0 +1,27 @@
+// Package cmd 存放程序的所有子命令
+package cmd
+
+import (
+	"gohub/pkg/helpers"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// Env 存储全局选项 --env 的值
+var Env string
+
+// RegisterGlobalFlags 注册全局选项（flag）
+func RegisterGlobalFlags(rootCmd *cobra.Command) {
+	rootCmd.PersistentFlags().StringVarP(&Env, "env", "e", "", "load .env file, example: --env=testing will use .env.testing file")
+}
+
+// RegisterDefaultCmd 注册默认命令
+func RegisterDefaultCmd(rootCmd *cobra.Command, subCmd *cobra.Command) {
+	cmd, _, err := rootCmd.Find(os.Args[1:])
+	firstArg := helpers.FirstElement(os.Args[1:])
+	if err == nil && cmd.Use == rootCmd.Use && firstArg != "-h" && firstArg != "--help" {
+		args := append([]string{subCmd.Use}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+}