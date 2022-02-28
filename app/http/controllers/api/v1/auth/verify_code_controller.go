summerblue/gohub

发送短信验证码

app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/requests"
 	"gohub/pkg/captcha"
 	"gohub/pkg/logger"
 	"gohub/pkg/response"
+	"gohub/pkg/verifycode"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,21 @@ func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
 		"captcha_id":    id,
 		"captcha_image": b64s,
 	})
+}
+
+// SendUsingPhone 发送手机验证码
+func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
+
+	// 1. 验证表单
+	request := requests.VeifyCodePhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.VeifyCodePhone); !ok {
+		return
+	}
+
+	// 2. 发送 SMS
+	if ok := verifycode.NewVerifyCode().SendSMS(request.Phone); !ok {
+		response.Abort500(c, "发送短信失败~")
+	} else {
+		response.Success(c)
+	}
 }

app/requests/verify_code_request.go
@@ -1,0 +1,51 @@
+package requests
+
+import (
+	"gohub/pkg/captcha"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type VeifyCodePhoneRequest struct {
+	CaptchaID     string `json:"captcha_id,omitempty" valid:"captcha_id"`
+	CaptchaAnswer string `json:"captcha_answer,omitempty" valid:"captcha_answer"`
+
+	Phone string `json:"phone,omitempty" valid:"phone"`
+}
+
+// VeifyCodePhone 验证表单，返回长度等于零即通过
+func VeifyCodePhone(data interface{}, c *gin.Context) map[string][]string {
+
+	// 1. 定制认证规则
+	rules := govalidator.MapData{
+		"phone":          []string{"required", "digits:11"},
+		"captcha_id":     []string{"required"},
+		"captcha_answer": []string{"required", "digits:6"},
+	}
+
+	// 2. 定制错误消息
+	messages := govalidator.MapData{
+		"phone": []string{
+			"required:手机号为必填项，参数名称 phone",
+			"digits:手机号长度必须为 11 位的数字",
+		},
+		"captcha_id": []string{
+			"required:图片验证码的 ID 为必填",
+		},
+		"captcha_answer": []string{
+			"required:图片验证码答案必填",
+			"digits:图片验证码长度必须为 6 位的数字",
+		},
+	}
+
+	errs := validate(data, rules, messages)
+
+	// 图片验证码
+	_data := data.(*VeifyCodePhoneRequest)
+	if ok := captcha.NewCaptcha().VerifyCaptcha(_data.CaptchaID, _data.CaptchaAnswer); !ok {
+		errs["captcha_answer"] = append(errs["captcha_answer"], "图片验证码错误")
+	}
+
+	return errs
+}

topcoder-club/club

app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/requests"
 	"gohub/pkg/captcha"
 	"gohub/pkg/logger"
 	"gohub/pkg/response"
+	"gohub/pkg/verifycode"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,21 @@ func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
 		"captcha_id":    id,
 		"captcha_image": b64s,
 	})
+}
+
+// SendUsingPhone 发送手机验证码
+func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
+
+	// 1. 验证表单
+	request := requests.VerifyCodePhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.VerifyCodePhone); !ok {
+		return
+	}
+
+	// 2. 发送 SMS
+	if ok := verifycode.NewVerifyCode().SendSMS(request.Phone); !ok {
+		response.Abort500(c, "发送短信失败~")
+	} else {
+		response.Success(c)
+	}
 }

app/requests/verify_code_request.go
@@ -1,0 +1,51 @@
+package requests
+
+import (
+	"gohub/pkg/captcha"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type VerifyCodePhoneRequest struct {
+	CaptchaID     string `json:"captcha_id,omitempty" valid:"captcha_id"`
+	CaptchaAnswer string `json:"captcha_answer,omitempty" valid:"captcha_answer"`
+
+	Phone string `json:"phone,omitempty" valid:"phone"`
+}
+
+// VerifyCodePhone 验证表单，返回长度等于零即通过
+func VerifyCodePhone(data interface{}, c *gin.Context) map[string][]string {
+
+	// 1. 定制认证规则
+	rules := govalidator.MapData{
+		"phone":          []string{"required", "digits:11"},
+		"captcha_id":     []string{"required"},
+		"captcha_answer": []string{"required", "digits:6"},
+	}
+
+	// 2. 定制错误消息
+	messages := govalidator.MapData{
+		"phone": []string{
+			"required:手机号为必填项，参数名称 phone",
+			"digits:手机号长度必须为 11 位的数字",
+		},
+		"captcha_id": []string{
+			"required:图片验证码的 ID 为必填",
+		},
+		"captcha_answer": []string{
+			"required:图片验证码答案必填",
+			"digits:图片验证码长度必须为 6 位的数字",
+		},
+	}
+
+	errs := validate(data, rules, messages)
+
+	// 图片验证码
+	_data := data.(*VerifyCodePhoneRequest)
+	if ok := captcha.NewCaptcha().VerifyCaptcha(_data.CaptchaID, _data.CaptchaAnswer); !ok {
+		errs["captcha_answer"] = append(errs["captcha_answer"], "图片验证码错误")
+	}
+
+	return errs
+}